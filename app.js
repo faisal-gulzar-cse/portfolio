7aed2e920344ae5b8bd2e2d93e68a7c5cc8b4f0a
faisal-gulzar-cse <159689077+faisal-gulzar-cse@users.noreply.github.com>
2024-02-12 20:02:30 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+// // Setting up the Variables
+// var bars = document.getElementById("nav-action");
+// var nav = document.getElementById("nav");
+
+
+// //setting up the listener
+// bars.addEventListener("click", barClicked, false);
+
+
+// //setting up the clicked Effect
+// function barClicked() {
+//   bars.classList.toggle('active');
+//   nav.classList.toggle('visible');
+// }
+
+
+
+
+
+
+
+
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
